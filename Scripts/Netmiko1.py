6a5bb3df5f8672f4ca96c2d3487dcd38f9f35fb6
palron12 <rpalacioso12@gmail.com>
2025-11-08 13:28:05 -0300

    Add scripts and requirements.txt to work

---
+++ Scripts/Netmiko1.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+
+cisco_881 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.137.131',
+    'username': 'cisco',
+    'password': 'pasapalabra1968$$',
+}
+
+net_connect = ConnectHandler(**cisco_881)
+
+output = net_connect.send_command('show ip int brief')
+print(output)
+
